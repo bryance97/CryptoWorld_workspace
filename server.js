15bc6e7d4aa06f182941e898692afb5bcda443ec
splaza28 <global-splaza28@gmail.com>
2021-12-05 20:09:11 -0600

    adding models folder with user js and updated server js file

--- server.js
+++ server.js
@@ -1,2 +1,8 @@
+var express = require('express');
+var session = require('express-session');
+//var User = require('./models/user');
+var hbs = require('express-handlebars'); 
+var path = require('path'); 
 
-}
+var app = express();
+app.set('port', 2000);
